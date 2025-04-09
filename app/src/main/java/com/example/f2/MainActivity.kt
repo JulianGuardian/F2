ff34cf1849d8caf836c280cc0eeb04d0fff45ddc
JulianGuardian <julguardian@proton.me>
2025-04-09 16:13:26 -0500

    "Feat: Added the correct display and styles to the Add Racer screen"

--- app/src/main/java/com/example/f2/MainActivity.kt
+++ app/src/main/java/com/example/f2/MainActivity.kt
@@ -7,11 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,10 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.f2.ui.theme.F2Theme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +41,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             F2Theme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    CreatePilot(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(25.dp)
+                    )
+                }
             }
         }
     }
@@ -40,7 +58,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Input( label: String, modifier: Modifier = Modifier) {
     var text by remember {
-        mutableStateOf(label)
+        mutableStateOf("")
     }
 
     Column (
@@ -48,12 +66,43 @@ fun Input( label: String, modifier: Modifier = Modifier) {
     ){
         Text(
             text = label,
-            modifier = Modifier.padding(8.dp)
+            fontSize = 18.sp,
+            modifier = modifier
+                .align(Alignment.Start)
+                .padding(start = 42.dp, bottom = 6.dp)
         )
-        TextField(
+        OutlinedTextField(
             value = text,
             onValueChange = { text = it },
+            shape = RoundedCornerShape(12.dp),
+            placeholder = {
+                Text(
+                    text = label,
+                    fontSize = 17.sp,
+                )
+            },
             modifier = modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 25.dp)
+                .padding(bottom = 25.dp)
+        )
+    }
+}
+
+@Composable
+fun CustomButton(text: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(20.dp),
+        modifier = modifier
+            .width(220.dp)
+            .height(80.dp)
+            .padding(10.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = (18.sp),
         )
     }
 }
@@ -62,51 +111,47 @@ fun Input( label: String, modifier: Modifier = Modifier) {
 fun CreatePilot(modifier: Modifier = Modifier) {
     Column (
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
+
         Image(
             painter = painterResource(id = R.drawable.logo_alt_f1),
             contentDescription = null,
             modifier = modifier
+                .size(120.dp)
+                .padding(bottom = 16.dp)
         )
 
         Image(
             painter = painterResource(id = R.drawable.default_profile_picture),
             contentDescription = null,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(
             modifier = modifier
         )
 
         Input(
             label = stringResource(R.string.Text_1),
-            modifier = modifier
+
         )
 
         Input(
             label = stringResource(R.string.Text_2),
-            modifier = modifier
         )
 
-        Button(
-            onClick = {
-                // Handle button click
-            }
-        ) {
-            Text(
-                text = stringResource(R.string.Button_1),
-                modifier = modifier
-            )
-        }
+        CustomButton(
+            text = stringResource(R.string.Button_1),
+            onClick = { /* Handle button click */ }
+        )
 
-        Button(
-            onClick = {
-                // Handle button click
-            }
-        ) {
-            Text(
-                text = stringResource(R.string.Button_2),
-                modifier = modifier
-            )
-        }
+        CustomButton(
+            text = stringResource(R.string.Button_2),
+            onClick = { /* Handle button click */ }
+        )
     }
 }
 
@@ -116,7 +161,7 @@ fun GreetingPreview() {
     F2Theme {
         CreatePilot(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .padding(16.dp)
         )
     }
